5a853d679c2e8bdc51c60a9ea1007c44a44fa3b7
mapkn3 <79608236848@ya.ru>
2023-08-15 01:04:44 +0400

    added some tests

--- src/test/java/course/concurrency/exams/blocking_queue/CustomBlockingQueueTest.java
+++ src/test/java/course/concurrency/exams/blocking_queue/CustomBlockingQueueTest.java
@@ -1,60 +1,160 @@
 package course.concurrency.exams.blocking_queue;
 
 import org.junit.jupiter.api.Test;
-import org.opentest4j.AssertionFailedError;
 
-import java.time.Duration;
+import java.util.Set;
+import java.util.concurrent.Executors;
 
+import static java.lang.Thread.State.WAITING;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CustomBlockingQueueTest {
 
-    private static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(1);
+    public static final long AWAIT_MILLIS = 1000L;
+
+    private Thread startNewThread(Runnable runnable) {
+        var thread = new Thread(runnable);
+        thread.start();
+
+        return thread;
+    }
 
     @Test
-    void enqueueSuccessful() {
+    void enqueueSuccessful() throws InterruptedException {
         var queue = new CustomBlockingQueue<>(1);
-        assertTimeoutPreemptively(DEFAULT_TIMEOUT, () -> queue.enqueue(new Object()));
+        var enqueueThread = startNewThread(() -> {
+            try {
+                queue.enqueue(new Object());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        enqueueThread.join(AWAIT_MILLIS);
 
+        assertNotEquals(WAITING, enqueueThread.getState());
         assertEquals(1, queue.size());
     }
 
     @Test
-    void dequeueSuccessful() {
+    void dequeueSuccessful() throws InterruptedException {
         var queue = new CustomBlockingQueue<>(1);
         var item = new Object();
-        assertTimeoutPreemptively(DEFAULT_TIMEOUT, () -> queue.enqueue(item));
+        var enqueueThread = startNewThread(() -> {
+            try {
+                queue.enqueue(item);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        enqueueThread.join(AWAIT_MILLIS);
+
+        assertNotEquals(WAITING, enqueueThread.getState());
         assertEquals(1, queue.size());
 
-        var dequeuedItem = assertTimeoutPreemptively(DEFAULT_TIMEOUT, queue::dequeue);
-        assertEquals(item, dequeuedItem);
+        var dequeueThread = startNewThread(() -> {
+            try {
+                var dequeuedItem = queue.dequeue();
+                assertEquals(item, dequeuedItem);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        dequeueThread.join(AWAIT_MILLIS);
+
+        assertNotEquals(WAITING, dequeueThread.getState());
         assertEquals(0, queue.size());
     }
 
     @Test
-    void secondEnqueueIsBlocked() {
+    void secondEnqueueIsBlocked() throws InterruptedException {
         var queue = new CustomBlockingQueue<>(1);
-        assertTimeoutPreemptively(DEFAULT_TIMEOUT, () -> queue.enqueue(new Object()));
-        var error = assertThrows(
-                AssertionFailedError.class,
-                () -> assertTimeoutPreemptively(DEFAULT_TIMEOUT, () -> queue.enqueue(new Object()))
-        );
+        var firstEnqueueThread = startNewThread(() -> {
+            try {
+                queue.enqueue(new Object());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        firstEnqueueThread.join(AWAIT_MILLIS);
 
+        assertNotEquals(WAITING, firstEnqueueThread.getState());
         assertEquals(1, queue.size());
-        assertEquals("execution timed out after 1000 ms", error.getMessage());
+
+        var secondEnqueueThread = startNewThread(() -> {
+            try {
+                queue.enqueue(new Object());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        secondEnqueueThread.join(AWAIT_MILLIS);
+
+        assertEquals(WAITING, secondEnqueueThread.getState());
+        assertEquals(1, queue.size());
     }
 
     @Test
-    void onlyDequeueIsBlocked() {
+    void onlyDequeueIsBlocked() throws InterruptedException {
         var queue = new CustomBlockingQueue<>(1);
-        var error = assertThrows(
-                AssertionFailedError.class,
-                () -> assertTimeoutPreemptively(DEFAULT_TIMEOUT, queue::dequeue)
-        );
+        var dequeueThread = startNewThread(() -> {
+            try {
+                queue.dequeue();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        dequeueThread.join(AWAIT_MILLIS);
 
+        assertEquals(WAITING, dequeueThread.getState());
         assertEquals(0, queue.size());
-        assertEquals("execution timed out after 1000 ms", error.getMessage());
+    }
+
+    @Test
+    void queueShouldWorkAsFIFO() throws InterruptedException {
+        var queue = new CustomBlockingQueue<Integer>(3);
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        assertEquals(3, queue.size());
+        assertEquals(1, queue.dequeue());
+        assertEquals(2, queue.dequeue());
+        assertEquals(3, queue.dequeue());
+        assertEquals(0, queue.size());
+    }
+
+    @Test
+    void shouldWorkInMultithreading() throws InterruptedException {
+        var executorService = Executors.newFixedThreadPool(6);
+        var queue = new CustomBlockingQueue<Integer>(2);
+        var expectedValues = Set.of(0, 1, 2);
+        for (int i = 0; i < 3; i++) {
+            executorService.submit(() -> {
+                try {
+                    var dequeuedItem = queue.dequeue();
+                    assertTrue(expectedValues.contains(dequeuedItem));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        for (int i = 0; i < 3; i++) {
+            var item = i;
+            executorService.submit(() -> {
+                try {
+                    queue.enqueue(item);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        executorService.shutdown();
+
+        assertTrue(executorService.awaitTermination(AWAIT_MILLIS, MILLISECONDS));
+        assertEquals(0, queue.size());
+
     }
 }
